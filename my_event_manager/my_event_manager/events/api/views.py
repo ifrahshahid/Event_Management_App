f23b263fe39fe0614a1129a72800b279f3f7c156
Ifrah <ifrahshahid2020@gmail.com>
2024-09-11 04:43:15 +0500

    Restrict update/delete to event owners

--- my_event_manager/my_event_manager/events/api/views.py
+++ my_event_manager/my_event_manager/events/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.exceptions import PermissionDenied
 from my_event_manager.events.models import Event
 from .serializers import EventSerializer
 
@@ -10,5 +11,19 @@ class EventViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action == 'list':
             return [AllowAny()]
-        else:
-            return [IsAuthenticated()]
+        return [IsAuthenticated()]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def perform_update(self, serializer):
+        event = self.get_object()
+        if event.owner != self.request.user:
+            raise PermissionDenied("You do not have permissions to update this event.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance.owner != self.request.user:
+            raise PermissionDenied("You do not have permissions to delete this event.")
+        instance.delete()
+
